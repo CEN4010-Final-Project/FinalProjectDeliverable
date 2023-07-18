23bb157d27695216312025c10f37ec7adde19f1a
Rohit <114543275+rohv25@users.noreply.github.com>
2023-07-18 12:36:41 -0400

    backend and db connection, .env file

---
+++ milestone-3/backend/backendServer.js
@@ -0,0 +1,100 @@
+// Importing necessary modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
+const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const axios = require("axios");
+const config = require("./config.js");
+
+// app.use
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '../FinalProjectDeliverable')));
+
+// FavoriteInfo schema
+const favoriteInfoSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    recipe: String,
+    //Add to schema
+  },
+  { versionKey: false }
+);
+
+//Set favorite model
+const Favorite = mongoose.model("favorite", favoriteInfoSchema);
+
+//Functions---------------------------------------------------------------> NEED TO TEST WITH POSTMAN
+app.post("/api/favorite", async (req, res) => {
+    const favorite = new Favorite ({
+        user_id : req.body.user_id,
+        recipe : req.body.recipe
+      });
+    
+      favorite.save()
+      console.log("Added Favorite");
+    //   console.log(`${favorite.user_id}`);
+    //   console.log(`${favorite.recipe}`);
+});
+
+app.delete("/api/favorite/:recipe", async (req, res) => {
+    var user_id = req.body.user_id;
+    var recipe = req.body.recipe;
+    try {
+      const favorite = await Favorite.findOneAndDelete({ user_id: user_id, recipe: recipe });
+      if (favorite) {
+        res.send("Deleted Favorite");
+      } else {
+        res.status(404).json({ message: "Favorite not found" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error deleting favorite");
+    }
+    //   console.log(`${favorite.user_id}`);
+    //   console.log(`${favorite.recipe}`);
+});
+
+app.get("/api/favorite/:recipe", async (req, res) => {
+    var user_id = req.body.user_id;
+    var recipe = req.body.recipe;
+    try {
+      const favorite = await Favorite.findOne({ user_id: user_id, recipe: recipe });
+      if (favorite) {
+        res.send(`Viewing ${favorite} from favorites`)
+      } else {
+        res.status(404).json({ message: "Favorite not found" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error viewing favorite");
+    }
+    //   console.log(`${favorite.user_id}`);
+    //   console.log(`${favorite.recipe}`);
+});
+
+app.get("/api/favorites", async (req, res) => {
+    try {
+        const favorite = await Favorite.find({ user_id: user_id });
+        if (favorite) {
+          res.send(`Viewing ${favorite} from favorites`)
+        } else {
+          res.status(404).json({ message: "Favorites not found" });
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("Error viewing favorites");
+      }
+    //   console.log(`${favorite.user_id}`);
+    //   console.log(`${favorite.recipe}`);
+});
+
+
+//Server
+app.listen(5678); //start the server
+console.log("Server is running...");
---
+++ milestone-3/backend/config.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster1.hmyq7ox.mongodb.net`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.log('Error connecting to database:', err);
+  } else {
+    console.log('Successfully connected to database!');
+    const collection = client.db("test").collection("test");
+    collection.findOne({}, (err, result) => {
+      if (err) {
+        console.log('Error querying database:', err);
+      } else {
+        console.log('Result:', result);
+        client.close();
+      }
+    });
+  }
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
